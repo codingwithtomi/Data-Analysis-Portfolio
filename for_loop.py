24d38ca165eb8d8af303a796f50e84bdc0d1716f
Tomisin Dada <tomisindada6@gmail.com>
2026-01-29 18:28:10 +0100

    Initial commit

---
+++ for_loop.py
@@ -0,0 +1,78 @@
+# fruits = ["apple", "banana", "orange", "guava", "watermelon"]
+
+# for fruit in fruits:
+#     # print(fruit)
+#     print("")
+
+# for number in range(20):
+#     # print("Banana", number + 1)
+#     print("")
+
+# matrix_a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# matrix_b = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+# matrix_c = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+# subarray_a = matrix_a[0]
+# first_element_in_subarray_a = subarray_a[0]
+# print(matrix_a[0][0])
+#      subarray_a
+
+# numbers = [1, 2, 3, 4, 5]
+
+# for number in numbers:
+#     print(number)
+
+# numbers = [1, 2, 3]
+
+# for number in numbers:
+#     print(number * 2)
+
+# word = "cat"
+
+# for cat in word:
+#     print(cat)
+
+
+# matrix = [[1, 2], [3, 4], [5, 6]]
+
+# for x in matrix:
+#     print(x)
+
+
+# for i in range(3):
+#     if i == 5:
+#         break
+#     print(i)
+# else:
+#     print("Done")
+
+
+# for i in range(3):
+#     print(i)
+#     if i == 2:
+#         break
+# else:
+#     print("Loop finished")
+
+
+# for number in range(3):
+#     print(number)
+# else:
+#     print("Done!")
+
+
+# for number in range(5):
+#     if number == 2:
+#         break
+#     print(number)
+# else:
+#     print("Done!")
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "orange":
+        print("Found orange!")
+        break
+else:
+    print("No orange found")
